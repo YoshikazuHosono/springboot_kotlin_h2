2943491b02c2267537a5fcf4d94e6dceedf9eba6
YoshikazuHosono <yyhhwork@gmail.com>
2020-07-20 22:43:39 +0900

    add

---
+++ src/main/kotlin/com/example/springboot_kotlin_h2/SpringbootKotlinH2Application.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_kotlin_h2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinH2Application
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinH2Application>(*args)
+}
